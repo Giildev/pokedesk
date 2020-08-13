ea012048f1f8252c1fa2e71d75e2c3fb5ab4c3af
vonhoro <64147799+vonhoro@users.noreply.github.com>
2020-08-13 09:58:00 -0400

    Faster
    Less api calls.

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
     pokemon();
     async function pokemon() {
         try {
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=807');
-            const data = await response.json()
-                const allPokemon = data.results;
-                console.log(allPokemon)
             let pokemonShow = [];
             let random;
             for (i = 0; i < 10; i +=1) {
                do{
-                  random = Math.floor(Math.random() * allPokemon.length)
-               }while(pokemonShow.includes(allPokemon[random].name))
-                pokemonShow.push(allPokemon[random].name)
+                  random = Math.floor((Math.random() * 806) + 1)
+               }while(pokemonShow.includes(random))
+                pokemonShow.push(random)
             }
             for (const pokemon of pokemonShow) {
                 const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
